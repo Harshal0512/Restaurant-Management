d060dc5a39e85578fc14b424b2d34208ebd009c2
Harshal Kulkarni <harshal.kulkarni0512@gmail.com>
2022-03-31 22:18:50 +0530

    Login Components

---
+++ restaurantmanagement/src/components/loginPageComponents/Login/Login.js
@@ -0,0 +1,169 @@
+import React, { useState, useEffect, useReducer, useRef } from "react";
+import Lottie from "lottie-web";
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+import {
+  LoginSuccessful,
+  LoginCredentialsError,
+} from "../../univComponents/Toast";
+import md5 from "md5";
+
+const emailReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.includes("@") };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.includes("@") };
+  }
+  return { value: "", isValid: false };
+};
+
+const passwordReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 4 };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.trim().length > 4 };
+  }
+  return { value: "", isValid: false };
+};
+
+const Login = (props) => {
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: null,
+  });
+
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: null,
+  });
+
+  const { isValid: emailisValid } = emailState;
+  const { isValid: passwordisValid } = passwordState;
+
+  useEffect(() => {
+    console.log("EFFECT RUNNING");
+
+    return () => {
+      console.log("EFFECT CLEANUP");
+    };
+  }, []);
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log("Checking form validity!");
+      setFormIsValid(emailisValid && passwordisValid);
+    }, 500);
+
+    return () => {
+      console.log("CLEANUP");
+      clearTimeout(identifier);
+    };
+  }, [emailisValid, passwordisValid]);
+
+  const emailChangeHandler = (event) => {
+    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
+  };
+
+  const passwordChangeHandler = (event) => {
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
+  };
+
+  const validateEmailHandler = () => {
+    dispatchEmail({ type: "INPUT_BLUR" });
+  };
+
+  const validatePasswordHandler = () => {
+    dispatchPassword({ type: "INPUT_BLUR" });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const credentials = {
+      email: emailState.value,
+      password: md5(passwordState.value),
+    };
+    fetch("http://localhost:7000/isLoginUserExists", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(credentials),
+    })
+      .then((response) => {
+        //const resp = response.json();
+        if (response.status === 200) {
+          LoginSuccessful();
+          props.onLogin(emailState.value, passwordState.value);
+        } else if (response.status === 401) LoginCredentialsError();
+      })
+      .catch((error) => {
+        LoginCredentialsError();
+      });
+  };
+
+  const container = useRef(null);
+
+  useEffect(() => {
+    Lottie.loadAnimation({
+      container: container.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("../UI/loginLottie.json"),
+    });
+  }, []);
+  return (
+    <>
+      <Card className={classes.login}>
+        <form onSubmit={submitHandler}>
+          <div
+            className={`${classes.control} ${
+              emailState.isValid === false ? classes.invalid : ""
+            }`}
+          >
+            <label htmlFor="email">E-Mail</label>
+            <input
+              type="email"
+              id="email"
+              value={emailState.value}
+              onChange={emailChangeHandler}
+              onBlur={validateEmailHandler}
+            />
+          </div>
+
+          <div
+            className={`${classes.control} ${
+              passwordState.IsValid === false ? classes.invalid : ""
+            }`}
+          >
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={passwordState.value}
+              onChange={passwordChangeHandler}
+              onBlur={validatePasswordHandler}
+            />
+          </div>
+
+          <div className={classes.actions}>
+            <Button
+              type="submit"
+              className={classes.btn}
+              disabled={!formIsValid}
+            >
+              Login
+            </Button>
+          </div>
+        </form>
+      </Card>
+
+      <div className={classes["lottie-container"]} ref={container}></div>
+    </>
+  );
+};
+
+export default Login;
